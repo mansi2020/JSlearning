6fd7ce6dc3cc9edd80b4ddbc0f42d9b18d518954
mansi2020 <sorathiyamansi11@gmail.com>
2023-03-25 12:00:05 +0530

    add caculation code

---
+++ calc.js
@@ -0,0 +1,52 @@
+/* calculation of Number */
+function calValue(OP1,Operand,OP2){
+	
+	switch(Operand){
+		case '+':
+			return OP1+OP2;
+
+		
+		case '-':
+			return OP1-OP2;
+			
+		case '*':
+			return OP1*OP2;
+			
+		case '/':
+			return OP1/OP2;
+		
+			} 
+	
+}
+
+console.log(calValue(13,'+',12));
+
+console.log(calValue(13,'-',12));
+
+console.log(calValue(12,'*',12));
+
+console.log(calValue(13,'/',12));
+
+
+
+
+	
+	
+	
+	
+	
+		
+		
+		
+ 
+
+
+
+
+
+
+
+
+
+
+
